9f0105b4c8936d99aad470ca6958b75e30ed7950
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-01-08 09:45:32 -0300

    update

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,6 +1,4 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -9,8 +7,10 @@ import { Button } from '../components/Button';
 
 import "../styles/auth.css"
 
+import { useAuth } from '../hooks/useAuth';
+
 export function NewRoom(){
-    const { user } = useContext(AuthContext)
+    const { user } = useAuth();
 
     return(
         <div id="page-auth">
@@ -22,7 +22,6 @@ export function NewRoom(){
             <main >
                 <div className='main-content'>
                     <img src={logoImg} alt="Letmeask"/>
-                    <h1>{user?.name}</h1>
                     <h2>Criar uma nova sala</h2>
                     <form>
                         <input 
