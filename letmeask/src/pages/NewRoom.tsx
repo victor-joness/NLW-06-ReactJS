52cc0adf14a101de689d0085828ef44c43b03a87
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-01-12 14:28:16 -0300

    update

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg'
@@ -12,6 +14,18 @@ import { useAuth } from '../hooks/useAuth';
 export function NewRoom(){
     const { user } = useAuth();
 
+    const [newRoom, setNewRoom] = useState("");
+
+    async function handleCreateRoom(event: FormEvent){
+        event.preventDefault();
+
+        if(newRoom.trim() === ""){
+            return;
+        }
+
+        
+    } 
+
     return(
         <div id="page-auth">
             <aside>
@@ -23,10 +37,12 @@ export function NewRoom(){
                 <div className='main-content'>
                     <img src={logoImg} alt="Letmeask"/>
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom} >
                         <input 
                         type="text" 
                         placeholder="Nome da Sala"
+                        onChange={event => setNewRoom(event.target.value)}
+                        value={newRoom}
                         />
                         <Button type="submit" >Criar sala</Button>
                     </form>
